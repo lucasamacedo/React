9a0b1c06c33813d6d6aa8c63b02e8009e2e89d14
lucasamacedo <lucas_am_@hotmail.com>
2021-05-14 07:12:38 -0300

    Adicionando categoria ao cards

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-
   constructor(props) {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Sem categoria";
+  }
+
+  _handleMudancaCategoria(evento) {
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
   }
 
   _handleMudancaTitulo(evento) {
@@ -21,17 +26,19 @@ class FormularioCadastro extends Component {
   _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
-      <form className="form-cadastro "
-        onSubmit={this._criarNota.bind(this)}
-      >
-        <select className="form-cadastro_input">
+      <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
+        <select
+          onChange={this._handleMudancaCategoria.bind(this)}
+          className="form-cadastro_input"
+        >
+          <option>Sem Categoria</option>
           {this.props.categorias.map((categoria, index) => {
-            return <option key={index}>{categoria}</option>
+            return <option key={index}>{categoria}</option>;
           })}
         </select>
         <input
